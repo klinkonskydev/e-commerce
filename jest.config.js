5ad90e679d254d9ada521ed40f94f0be14876774
klinkonsky <klinkonskydev@gmail.com>
2022-07-22 08:57:22 -0300

    feature: jest-styled-components (toHaveStyleRule) declaration file

--- jest.config.js
+++ jest.config.js
@@ -8,11 +8,15 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
-  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/pages/*',
+    '!src/types/*',
+    '!src/styles/**/*'
+  ],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom'
 }
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig)
